b212cd8a30715615319afa318428e6bfa5c9bb48
mahmud035 <mahamudulhasan4148@gmail.com>
2025-02-11 06:42:36 +0600

    Added CI/CD setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // ✅ Enables global expect
+    environment: 'jsdom', // ✅ Simulates browser environment
+    setupFiles: './src/setupTests.ts', // ✅ Setup file for jest-dom
+  },
+});
